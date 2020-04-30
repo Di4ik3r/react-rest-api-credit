cbbeb85cec310250dd27ed5f8629d2d4378259d4
Di4ik3r <di4iker@gmail.com>
2020-04-30 20:57:07 +0300

    added cors for credit-router

--- routes/creditRouter.js
+++ routes/creditRouter.js
@@ -1,7 +1,10 @@
 const express = require("express")
 const router = express.Router()
+const cors = require("cors")
 
 const creditController = require("../controllers/creditController")
+
+router.use(cors())
 
 router.get("/", creditController.getAll)
 router.get("/:id", creditController.getById)
